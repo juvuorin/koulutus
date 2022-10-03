9d78a1e429022a29209422233b6a8d944f0ea785
Juuso Vuorinen <juvuorin@gmail.com>
2022-10-03 13:42:12 +0300

    ok

---
+++ testi.js
@@ -0,0 +1,13 @@
+/* Tee funktio, joka saa parametrina kokonaisluvun. Jos luku on suurempi kuin 100, funktio
+palauttaa: ”syötit luvun, joka on suurempi kuin 100”. Jos luku on 100 tai pienempi,
+palautetaan “” */
+
+
+function funktio(luku) {
+
+  if (luku <= 100) {
+    return ""
+  } 
+  return "syötit luvun, joka on suurempi kuin 100"
+  }
+}
